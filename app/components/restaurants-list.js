f1170381e1a25da22b2ed92b152cd7be86581f7f
JacobChen1994 <jacobsenpai@outlook.com>
2016-12-10 18:48:04 +0000

    Added filter by search box query feature

--- app/components/restaurants-list.js
+++ app/components/restaurants-list.js
@@ -3,24 +3,41 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
 	filters: [],
+	searchRefine: "",
 	filterPanelDisplay: false,
 
 	willDestroyElement() {
 		var filters = this.get('filters');
 		filters.clear();
+
+		var searchRefine = this.get('searchRefine');
+		searchRefine = "";
 	},
 
 	filteredRestaurants: function() {
 		var filters = this.get('filters');
-		return this.get('restaurants').filter(function(restaurant) {
-			if (filters.length < 1) {
-				return true;
-			}
-			else {
-				return filters.includes(restaurant.data.type);
-			}
-		});
-	}.property('filters.[]'),
+		var searchRefine = this.get('searchRefine');
+
+		return this.get('restaurants')
+			.filter(function(restaurant) {
+				if (filters.length < 1) {
+					return true;
+				}
+				else {
+					return filters.includes(restaurant.data.type);
+				}
+			}).filter(function(restaurant) {
+				if (searchRefine == "") {
+					return true;
+				}
+				else {
+					let restaurantName = restaurant.data.name.toLowerCase();
+					let query = searchRefine.toLowerCase();
+
+					return restaurantName.includes(query);
+				}
+			});
+	}.property('filters.[]', 'searchRefine'),
 
 	actions: {
 		filter(value) {
