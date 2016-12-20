8ffc28022428a9291994ff9bac26e42d05146b12
JacobChen1994 <jacobsenpai@outlook.com>
2016-12-20 14:36:39 +0000

    Updated Styles and Accesibility
    - Updated tags, images, colours.
    - No fingerprint production images

--- app/helpers/generate-stars.js
+++ app/helpers/generate-stars.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export function generateStars(params/*, hash*/) {
 	var [stars] = params;
-	var html = `<div class="stars" title="Rating. ${stars} stars" tabindex="0">`;
+	var html = `<div class="stars" title="Rating. ${stars} stars">`;
 	for (var i = 0; i < stars; i++) {
-		html += `<img src="/images/gold_star.svg.png">`;
+		html += `<img src="/images/gold_star.svg.png" role="presentation">`;
 	}
 	html += '</div>';
   	return Ember.String.htmlSafe(html);
